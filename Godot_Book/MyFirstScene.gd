99b746a61c6c75bac8e6f2dfba10a8d2709963dd
Kentaro Urata <kentu@microsoft.com>
2019-11-23 04:29:11 -0800

    moving the key handler to root

--- Godot_Book/MyFirstScene.gd
+++ Godot_Book/MyFirstScene.gd
@@ -1,5 +1,26 @@
 extends Node2D
 
 
+const MOVEMENT_SPEED = 50 # pixels per second
+
+# Declare member variables here. Examples:
+var sprite_node
+
+func _ready():
+	sprite_node = get_node("Sprite")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_direction = 0 # 0 is no movement, 1 is right, -1 is left
+
+	if Input.is_action_pressed("ui_left"):
+		input_direction = -1
+	elif Input.is_action_pressed("ui_right"):
+		input_direction = 1
+
+	sprite_node.position.x += input_direction * MOVEMENT_SPEED * delta
+
+
 func _on_Button_pressed():
 	$Sprite.visible = not $Sprite.visible
+
--- Godot_Book/Sprite.gd
+++ Godot_Book/Sprite.gd
@@ -1,21 +1,21 @@
 extends Sprite
 
-const MOVEMENT_SPEED = 50 # pixels per second
-
-# Declare member variables here. Examples:
-var input_direction = 0 # 0 is no movement, 1 is right, -1 is left
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position.x += input_direction * MOVEMENT_SPEED * delta
-
-func _input(event):
-	if event.is_action_pressed("ui_left"):
-		input_direction = -1
-	elif event.is_action_pressed("ui_right"):
-		input_direction = 1
-	elif event.is_action_released("ui_left"):
-		input_direction = 0
-	elif event.is_action_released("ui_right"):
-		input_direction = 0
+#const MOVEMENT_SPEED = 50 # pixels per second
+#
+## Declare member variables here. Examples:
+#var input_direction = 0 # 0 is no movement, 1 is right, -1 is left
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	position.x += input_direction * MOVEMENT_SPEED * delta
+#
+#func _input(event):
+#	if event.is_action_pressed("ui_left"):
+#		input_direction = -1
+#	elif event.is_action_pressed("ui_right"):
+#		input_direction = 1
+#	elif event.is_action_released("ui_left"):
+#		input_direction = 0
+#	elif event.is_action_released("ui_right"):
+#		input_direction = 0
